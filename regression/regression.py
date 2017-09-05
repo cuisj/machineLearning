08563d267614c9a094092739420fd2b8586a39c6
cuisj <lingfengsj@126.com>
2017-09-05 14:57:45 +0800

    实现局部加权线性回归

--- regression/regression.py
+++ regression/regression.py
@@ -35,7 +35,7 @@ def standRegres(xArr, yArr):
     # 返回回归系数
     return ws
 
-def showFigure(xArr, yArr, ws):
+def showStandRegresFigure(xArr, yArr, ws):
     xMat = mat(xArr)
     yMat = mat(yArr)
 
@@ -59,8 +59,67 @@ def correlationCoefficient(xArr, yArr, ws):
 
     return  corrcoef(yHat.T, yMat)
 
-if __name__ == '__main__':
+def testStandRegres():
     xArr, yArr = loadDataSet('ex0.txt')
     ws = standRegres(xArr, yArr)
     print correlationCoefficient(xArr, yArr, ws)
-    showFigure(xArr, yArr, ws)
+    showStandRegresFigure(xArr, yArr, ws)
+
+
+# 局部加权线性回归
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))
+
+    # 创建对角加权矩阵, 使用高斯核
+    for j in range(m):
+        diffMat = testPoint - xMat[j,:]
+        weights[j,j] = exp(diffMat*diffMat.T/(-2.0*k**2))
+
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0:
+        print "xTx不可逆"
+        return
+
+    ws = xTx.I * (xMat.T * (weights * yMat))
+
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+
+    return yHat
+
+def showLWLRFigure(xArr, yArr, k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # 原始点集
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0], s=2, c='red')
+
+    yHat = lwlrTest(xArr, xArr, yArr, k)
+
+    srtInd = xMat[:, 1].argsort(0)
+    xSort = xMat[srtInd][:,0]
+
+    ax.plot(xSort[:,1].flatten().A[0], yHat[srtInd].flatten())
+
+    plt.show()
+
+
+def testLWLR():
+    xArr, yArr = loadDataSet('ex0.txt')
+    # print lwlr(xArr[0], xArr, yArr, 0.01)
+    # print lwlrTest(xArr, xArr, yArr, 0.01)
+    showLWLRFigure(xArr, yArr, 0.01)
+
+if __name__ == '__main__':
+    pass
